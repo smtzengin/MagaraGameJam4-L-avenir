c2d5287c1c124e7ed9fbaad4ce83a450f9c920b1
Samet Zengin <smt.zngn02@gmail.com>
2022-02-20 17:48:47 +0300

    Level3Design
    Level3 Design..

--- LavenirGamesMAGARAJAM4/Assets/GameFolders/Scripts/Concretes/Movements/Crouch.cs
+++ LavenirGamesMAGARAJAM4/Assets/GameFolders/Scripts/Concretes/Movements/Crouch.cs
@@ -18,29 +18,29 @@ namespace LavenirGamesMAGARAJAM4.Movements
         private void Awake()
         {
             _characterAnimation = GetComponent<CharacterAnimation>();
-            _colSize = gameObject.GetComponent<BoxCollider2D>().size;
-            _offset = gameObject.GetComponent<BoxCollider2D>().offset;
+            _colSize = gameObject.GetComponent<CapsuleCollider2D>().size;
+            _offset = gameObject.GetComponent<CapsuleCollider2D>().offset;
         }
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.C))
             {
-                _colSize = gameObject.GetComponent<BoxCollider2D>().size;
-                _offset = gameObject.GetComponent<BoxCollider2D>().offset;
+                _colSize = gameObject.GetComponent<CapsuleCollider2D>().size;
+                _offset = gameObject.GetComponent<CapsuleCollider2D>().offset;
                 if (_pushCButton == 0)
                 {
                     _pushCButton += 1;
                     _isCrouch = true;
                     _characterAnimation.CrouchAnimation(_isCrouch);
-                    gameObject.GetComponent<BoxCollider2D>().size = new Vector2(_colSize.x, _colSize.y - 0.5f);
-                    gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(_offset.x, _offset.y - 0.25f);
+                    gameObject.GetComponent<CapsuleCollider2D>().size = new Vector2(_colSize.x, _colSize.y - 1.1f);
+                    gameObject.GetComponent<CapsuleCollider2D>().offset = new Vector2(_offset.x, _offset.y - 0.50f);
                 }
                 else if (_pushCButton >= 0)
                 {
                     _isCrouch = false;
                     _characterAnimation.CrouchAnimation(_isCrouch);
-                    gameObject.GetComponent<BoxCollider2D>().size = new Vector2(_colSize.x, _colSize.y + 0.5f);
-                    gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(_offset.x, _offset.y + 0.25f);
+                    gameObject.GetComponent<CapsuleCollider2D>().size = new Vector2(_colSize.x, _colSize.y + 1.1f);
+                    gameObject.GetComponent<CapsuleCollider2D>().offset = new Vector2(_offset.x, _offset.y + 0.50f);
                     _pushCButton = 0;
 
                 }
